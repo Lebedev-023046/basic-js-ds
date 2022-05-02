022fd3590cefccf8d17ff9f4d8adfcd3ca34e74f
lebedev-023046 <lebedev.023046@gmail.com>
2022-05-02 19:31:00 +0300

    2/4

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -15,21 +15,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor () {
+    this.head = null,
+    this.tail = null,
+    this.length = 0
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  getUnderlyingList() {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.head
+    // console.log(result)
+  }
+
+  enqueue(value) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    const node = new ListNode(value)
+
+    if (this.head) {
+      this.tail.next = node
+      this.tail = node
+    }else {
+      this.head = node
+      this.tail = node
+    }
+    this.length++
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    const current = this.head
+    this.head = this.head.next
+    this.length--
+    return current.value
   }
 }
+
+const queue = new Queue()
+
+queue.enqueue(1); // adds the element to the queue
+queue.enqueue(3); // adds the element to the queue
+queue.enqueue(4);
+queue.enqueue(5);
+queue.enqueue(6);
+queue.enqueue(7);
+queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+console.log(queue.getUnderlyingList()) // returns { value: 3, next: null }
 
 module.exports = {
   Queue
